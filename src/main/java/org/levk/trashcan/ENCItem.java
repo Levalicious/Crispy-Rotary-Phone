61525ee652e2e742fb088272fddbb734086cd1d0
Lev Knoblock <levknoblock@gmail.com>
2018-07-19 01:02:28 -0400

    Initial commit

---
+++ src/main/java/org/levk/trashcan/ENCItem.java
@@ -0,0 +1,58 @@
+package org.levk.trashcan;
+
+import java.util.Arrays;
+
+public class ENCItem {
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    private byte[] data;
+
+    public ENCItem(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getEncData() {
+        return data;
+    }
+
+    public String toString() {
+        char[] hexChars = new char[data.length * 2];
+        for ( int j = 0; j < data.length; j++ ) {
+            int v = data[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ENCItem)) {
+            return false;
+        }
+
+        if (!Arrays.equals(this.data, ((ENCItem) o).getEncData())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isList() {
+        if (isEncoded()) {
+            if (data[0] == 0x00) return true;
+        }
+
+        return false;
+    }
+
+    /* TODO: Implement a faster/less costly method to check this */
+    public boolean isEncoded() {
+        try {
+            TRENC.decode(data);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/org/levk/trashcan/ENCList.java
@@ -0,0 +1,25 @@
+package org.levk.trashcan;
+
+import java.util.ArrayList;
+
+public class ENCList extends ArrayList<ENCItem> {
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ENCList)) {
+            return false;
+        }
+
+        if (((ENCList) o).size() != this.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < this.size(); i++) {
+            if (!this.get(i).equals(((ENCList) o).get(i))) {
+                System.out.println("Byte array " + i + " is different.");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
